6a3f5c858b2e3f9cb505870862c419d29ecd85ad
sawirricardo <sawir.ricardo@gmail.com>
2022-05-27 13:45:25 +0700

    fix namespace

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 use Api2Pdf\Api2Pdf as Client;
-use Sawirricardo\Api2pdf\Laravel\Api2pdf;
+use Sawirricardo\Laravel\Api2pdf\Api2pdf;
 
 if (! function_exists('api2pdf')) {
     function api2pdf(): Client
