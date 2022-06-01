c0bc929d318bdadf91f664ba6ca620f3d77037c8
sawirricardo <sawir.ricardo@gmail.com>
2022-06-01 09:49:05 +0700

    remove comment

--- src/Api2pdfServiceProvider.php
+++ src/Api2pdfServiceProvider.php
@@ -9,11 +9,6 @@ class Api2pdfServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-api2pdf')
             ->hasConfigFile();
