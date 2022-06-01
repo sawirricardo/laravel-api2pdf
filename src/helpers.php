8d38125c8d85b46ce4072d77a5d3d1ca083f91ef
sawirricardo <sawir.ricardo@gmail.com>
2022-06-01 09:49:12 +0700

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,11 @@
 <?php
 
+use Sawirricardo\Api2pdf\Laravel\Api2pdf as Client;
 use Sawirricardo\Api2pdf\Laravel\Facades\Api2pdf;
 
 if (! function_exists('api2pdf')) {
-    function api2pdf(): Api2pdf
+    function api2pdf(): Client
     {
-        return app(Api2pdf::class);
+        return app(Api2pdf::class)::new();
     }
 }
